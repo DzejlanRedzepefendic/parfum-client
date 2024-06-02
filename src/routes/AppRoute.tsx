8fb742497168252b58784bdddf7c587ef3ee34d7
dzelaldupljak <dzelal.dupljak@omnistreak.com>
2024-06-03 00:55:34 +0200

    added logout function

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,57 +1,58 @@
 import React from 'react';
-import {  Routes, Route, Navigate } from 'react-router-dom';
-import {  useAuth } from '../context/AuthContext';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import ProtectedRoute from './ProtectedRoute';
-import { Container } from '@mui/material';
+import {  Container } from '@mui/material';
 import BottomNavBar from '../components/BottomNavBar';
 import { Home } from '../pages/Home';
 import { Article } from '../pages/Article';
 import { Profile } from '../pages/Profile';
 import Login from '../pages/Login';
+import MenuAppBar from '../components/MenuAppBar';
 
 const AppRoute: React.FC = () => {
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
 
-    const { isLoggedIn } = useAuth();
-    const notShow = ['/login'].includes(location.pathname);
-
+  const notShow = ['/login'].includes(location.pathname);
 
   return (
-    <Container sx={{minHeight:'100vh'}} maxWidth="sm" style={{ paddingBottom: notShow ? '56px' : '0' }}>
-        <Routes>
-        <Route path="/login" element={
-            <Login/>
-        } />
-      <Route
-        path="/home"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/article"
-        element={
-          <ProtectedRoute>
-            <Article />
-          </ProtectedRoute>
-        }
-      />
-       <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/"
-        element={isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />}
-      />
-      <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      {!notShow  &&  <BottomNavBar />}
+    <Container
+      sx={{ minHeight: '100vh',width:'100%',backgroundColor:'#f5f5f5' }}
+      maxWidth="sm"
+      style={{ paddingBottom: notShow ? '0' : '56px',paddingLeft:0,paddingRight:0 }}
+    >
+      {isLoggedIn && <MenuAppBar/>}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/article"
+          element={
+            <ProtectedRoute>
+              <Article />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      {!notShow && <BottomNavBar />}
     </Container>
   );
 };
